74ed7eabef731c8244ef58eebf65ed700fe48416
Daniel Newby <daniel.newby@apdcomms.co.uk>
2020-05-15 16:19:15 +0100

    added buildspec and status controller for testing

---
+++ BrainDumpApi/Controllers/StatusController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BrainDumpApi.Controllers
+{
+	[Route("api/[controller]")]
+	public class StatusController : ControllerBase
+	{
+		// GET: /<controller>/
+		public IActionResult Index()
+		{
+			return Ok("Its ok");
+		}
+	}
+}
